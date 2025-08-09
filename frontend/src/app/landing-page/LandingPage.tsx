63a900bcca4bf1e6e6f57bb0d23c31a0e74701d6
PatrickFan <124469718+lighteningAB@users.noreply.github.com>
2025-08-09 13:55:06 +0100

    structural changes

---
+++ frontend/src/app/landing-page/LandingPage.tsx
@@ -0,0 +1,18 @@
+import { Input, Flex, Box} from "@chakra-ui/react"
+
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+const Page = () => {
+  return (
+    <Flex minH="100vh" align="center" justify="center">
+      <Box width={"50%"}>
+        <Input placeholder="Enter your email" mb={4} />
+        {days.map((text, idx) => (
+          <Input key={idx} placeholder={`Enter your tasks for ${text}` } mb={2} />
+        ))}
+      </Box>
+    </Flex>
+  )
+}
+
+export default Page;
