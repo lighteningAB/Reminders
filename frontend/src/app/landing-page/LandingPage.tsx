85c1bde8cf1abe05756cd988ad5d84a9c2056e2a
Patrick Fan <fanpatrick8@gmail.com>
2025-08-09 22:05:53 +0100

    added modal and open button

--- frontend/src/app/landing-page/LandingPage.tsx
+++ frontend/src/app/landing-page/LandingPage.tsx
@@ -1,10 +1,15 @@
-import { Heading, Box, Flex } from "@chakra-ui/react"
+'use client';
+
+import { Heading, Box, Flex, Button, useDisclosure, Modal, ModalOverlay, ModalContent, ModalBody } from "@chakra-ui/react"
 import DayBlock from "./DayBlock";
+import ModalForm from "../modal-form/ModalForm";
 
 const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
 
 const LandingPage = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return(
     <Flex width ={"100%"} align="center" justify={"center"} mt="5%" direction="column">
       <Box mb={4} border = "1px solid red" padding = "3%" width = "50%" display="flex" justifyContent="center" alignItems="center" borderRadius="30" >
@@ -21,6 +26,17 @@ const LandingPage = () => {
       <Box mb={4} border = "1px solid red" padding = "1%" width = "80%" borderRadius="30">
         <Heading size="md">Special Tasks: </Heading>
       </Box>
+      <Button colorScheme="teal" mt={4} onClick={onOpen}>
+        Add Reminders
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalBody>
+            <ModalForm />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </Flex>
   )
 }
